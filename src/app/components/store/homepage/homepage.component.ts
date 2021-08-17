9f2247c79c24122b0c96ec15cda1b25786b54952
ilyas ahmed <ilyasahmed081@gmail.com>
2021-08-17 22:40:41 +0500

    homepage done

--- src/app/components/store/homepage/homepage.component.ts
+++ src/app/components/store/homepage/homepage.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomepageComponent implements OnInit {
 
+  list: any = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.list = [
+      { image: "assets/img/a.jpg" },
+      { image: "assets/img/b.jpg" },
+      { image: "assets/img/c.png" },
+      { image: "assets/img/d.jpg" },
+      { image: "assets/img/e.jpg" },
+      { image: "assets/img/f.jpg" },
+      { image: "assets/img/g.jpg" },
+      { image: "assets/img/h.jpg" },
+    ]
   }
 
 }
