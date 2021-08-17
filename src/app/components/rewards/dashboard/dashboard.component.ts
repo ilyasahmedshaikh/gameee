0fdb3bafaaedfaa137ec88e17e362b7c89b7f22c
ilyas ahmed <ilyasahmed081@gmail.com>
2021-08-18 01:57:36 +0500

    store rewards screens with popups done

--- src/app/components/rewards/dashboard/dashboard.component.ts
+++ src/app/components/rewards/dashboard/dashboard.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+  list: any = [
+    { id: 1,
+      count: "100"
+    },
+    { id: 2,
+      count: "200"
+    },
+    { id: 3,
+      count: "300"
+    },
+    { id: 4,
+      count: "400"
+    },
+    { id: 5,
+      count: "500"
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
 }
